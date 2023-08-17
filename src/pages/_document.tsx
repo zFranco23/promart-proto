a93c4a71c8108531249868be52c4897652f739dc
zFranco46 <franco.hermenegildo@promart.pe>
2023-08-17 14:56:54 -0500

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>B2B protos</title>
         <link
           rel="preload"
           crossOrigin="anonymous"
